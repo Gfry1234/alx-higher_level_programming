e08cd6d87c8021d36e178a682c3974855768ebbb
Gfry1234 <iamgfry@yahoo.com>
2023-01-07 21:14:06 -0800

    Technical interview preparation:
    You are not allowed to google anything
    Whiteboard first
    Write a function in C that checks if a singly linked list is a palindrome.
    
    Prototype: int is_palindrome(listint_t **head);
    Return: 0 if it is not a palindrome, 1 if it is a palindrome
    An empty list is considered a palindrome
    carrie@ubuntu:0x03$ cat lists.h
    
    /**
     * struct listint_s - singly linked list
     * @n: integer
     * @next: points to the next node
     *
     * Description: singly linked list node structure
     * for project
     */
    typedef struct listint_s
    {
        int n;
        struct listint_s *next;
    } listint_t;
    
    size_t print_listint(const listint_t *h);
    listint_t *add_nodeint_end(listint_t **head, const int n);
    void free_listint(listint_t *head);
    
    int is_palindrome(listint_t **head);
    
    carrie@ubuntu:0x03$
    carrie@ubuntu:0x03$ cat linked_lists.c
    
    /**
     * print_listint - prints all elements of a listint_t list
     * @h: pointer to head of list
     * Return: number of nodes
     */
    size_t print_listint(const listint_t *h)
    {
        const listint_t *current;
        unsigned int n; /* number of nodes */
    
        current = h;
        n = 0;
        while (current != NULL)
        {
            printf("%i\n", current->n);
            current = current->next;
            n++;
        }
    
        return (n);
    }
    
    /**
     * add_nodeint_end - adds a new node at the end of a listint_t list
     * @head: pointer to pointer of first node of listint_t list
     * @n: integer to be included in new node
     * Return: address of the new element or NULL if it fails
     */
    listint_t *add_nodeint_end(listint_t **head, const int n)
    {
        listint_t *new;
        listint_t *current;
    
        current = *head;
    
        new = malloc(sizeof(listint_t));
        if (new == NULL)
            return (NULL);
    
        new->n = n;
        new->next = NULL;
    
        if (*head == NULL)
            *head = new;
        else
        {
            while (current->next != NULL)
                current = current->next;
            current->next = new;
        }
    
        return (new);
    }
    
    /**
     * free_listint - frees a listint_t list
     * @head: pointer to list to be freed
     * Return: void
     */
    void free_listint(listint_t *head)
    {
        listint_t *current;
    
        while (head != NULL)
        {
            current = head;
            head = head->next;
            free(current);
        }
    }
    carrie@ubuntu:0x03$
    carrie@ubuntu:0x03$ cat 13-main.c
    
    /**
     * main - check the code for
     *
     * Return: Always 0.
     */
    int main(void)
    {
        listint_t *head;
    
        head = NULL;
        add_nodeint_end(&head, 1);
        add_nodeint_end(&head, 17);
        add_nodeint_end(&head, 972);
        add_nodeint_end(&head, 50);
        add_nodeint_end(&head, 98);
        add_nodeint_end(&head, 98);
        add_nodeint_end(&head, 50);
        add_nodeint_end(&head, 972);
        add_nodeint_end(&head, 17);
        add_nodeint_end(&head, 1);
        print_listint(head);
    
        if (is_palindrome(&head) == 1)
            printf("Linked list is a palindrome\n");
        else
            printf("Linked list is not a palindrome\n");
    
        free_listint(head);
    
        return (0);
    }
    carrie@ubuntu:0x03$
    carrie@ubuntu:0x03$ gcc -Wall -Werror -Wextra -pedantic 13-main.c linked_lists.c 13-is_palindrome.c -o palindrome
    carrie@ubuntu:0x03$ ./palindrome
    1
    17
    972
    50
    98
    98
    50
    972
    17
    1
    Linked list is a palindrome
    carrie@ubuntu:0x03

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,40 +1,86 @@
 #include "lists.h"
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
-  * is_palindrome - A function that checks if a list is a palindrome.
-  * @head: The pointer to the head of the list.
-  * Return: 0 if list not a palindrome, 1 if it is.
-  */
-int is_palindrome(listint_t **head)
-{
-	listint_t *tmp = *head;
-	int nodes = 0, i = 0, *array = NULL;
+ * is_palindrome - chexk if a list is palindrome
+ * Description:
+ * The approach uses the fact that a list
+ * - is palindrom if:
+ *   its first half is thesame as the reverse
+ *   of its last half
+ * - else its not a palindrom
+ * The middle element is found using the
+ * tortoise hare algorithm
+ * In the case where the list has odd length
+ * the middle element is disregarded
+ *
+ * @list: pointer to list
+ * Return: 1 if true 0 otherwise
+*/
 
-	if (*head == NULL || head == NULL || (*head)->next == NULL)
+int is_palindrome(listint_t **list)
+{
+	int result = 1;
+	listint_t *fast, *slow, *h1, *h2, *tmp;
+
+	if (!list)
+		return (0);
+
+	/* if list length is less than 2 */
+	if (!(*list) || !((*list)->next))
 		return (1);
-	while (tmp)
+
+	/* find the middle node */
+	slow = fast = *list;
+	while (fast && fast->next)
 	{
-		nodes++;
-		tmp = tmp->next;
+		slow = slow->next;
+		fast = fast->next->next;
 	}
-	array = malloc(sizeof(int) * nodes);
-	tmp = *head;
-	while (tmp)
+	if (fast)
+		slow = slow->next;
+
+	/* reverse the last half */
+	h2 = reverse_listint(&slow);
+	tmp = h2;
+
+	/* compare the two halves */
+	h1 = *list;
+	while (h2)
 	{
-		array[i++] = tmp->n;
-		tmp = tmp->next;
-	}
-	for (i = 0; i < nodes / 2; i++)
-	{
-		if (array[i] != array[nodes - 1 - i])
+		if (h2->n != h1->n)
 		{
-			free(array);
-			return (0);
+			result = 0;
+			break;
 		}
+		h1 = h1->next;
+		h2 = h2->next;
 	}
-	free(array);
-	return (1);
+
+	/* rever the half back to original */
+	reverse_listint(&tmp);
+		return (result);
+}
+
+/**
+ * reverse_listint - reverses a linked list
+ * @h: head of the list
+ *
+ * Return: pointer to the reversed
+ */
+listint_t *reverse_listint(listint_t **h)
+{
+	listint_t *tmp2, *tmp1;
+
+	if (!h || !(*h))
+		return (NULL);
+	tmp1 = (*h)->next;
+	(*h)->next = NULL;
+	while (tmp1)
+	{
+		tmp2 = *h;
+		*h = tmp1;
+		tmp1 = (*h)->next;
+		(*h)->next = tmp2;
+	}
+	return (*h);
 }
