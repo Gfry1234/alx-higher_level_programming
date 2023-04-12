1c2957f21d56afb3c855eeec7ffa35f3c84731f9
root <iamgfry@yahoo.com>
2023-04-12 10:49:05 -0700

    Write a function that converts a number from base 10 to another base passed as argument:
    Prototype: exports.converter = function (base)
    You are not allowed to import any file
    You are not allowed to declare any new variable (var, let, etc..)
    guillaume@ubuntu:~/0x13$ cat 10-main.js
    const converter = require('./10-converter').converter;
    
    let myConverter = converter(10);
    
    console.log(myConverter(2));
    console.log(myConverter(12));
    console.log(myConverter(89));
    
    myConverter = converter(16);
    
    console.log(myConverter(2));
    console.log(myConverter(12));
    console.log(myConverter(89));
    
    guillaume@ubuntu:~/0x13$ ./10-main.js
    2
    12
    89
    2
    c
    59
    guillaume@ubuntu:~/0x13$

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,8 +1,2 @@
-
 #!/usr/bin/node
-// defines function that converts a number from base 10 to `base`
-exports.converter = function (base) {
-  return function (value) {
-    return value.toString(base);
-  };
-};
+exports.converter = function (base) { return num => num.toString(base); };
