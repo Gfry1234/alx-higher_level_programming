624c4a58707182ba85624b1a90096133aece7030
root <iamgfry@yahoo.com>
2023-04-12 11:13:10 -0700

    Write a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
    Your script must import dict from the file 101-data.js
    In the new dictionary:
    A key is a number of occurrences
    A value is the list of user ids
    Print the new dictionary at the end
    guillaume@ubuntu:~/0x13$ cat 101-data.js
    exports.dict = {
      89: 1,
      90: 2,
      91: 1,
      92: 3,
      93: 1,
      94: 2
    };
    guillaume@ubuntu:~/0x13$ ./101-sorted.js
    { '1': [ '89', '91', '93' ], '2': [ '90', '94' ], '3': [ '92' ] }
    guillaume@ubuntu:~/0x13$

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
+// import dict from data file; creates+prints new dict {frequency: list of ids}
 const dict = require('./101-data').dict;
-const dKeys = Object.keys(dict);
-const values = Object.values(dict);
-let matched;
-const result = {};
-// loop over the values
-for (let i = 0; i < values.length; i++) {
-  result[JSON.stringify(values[i])] = [];
-  matched = dKeys.filter(key => dict[key] === values[i]);
-  matched.forEach(item => result[JSON.stringify(values[i])].push(item));
+
+const sortByFrequency = {};
+for (const key in dict) {
+  if (dict[key] in sortByFrequency) {
+    sortByFrequency[dict[key]].push(key);
+  } else {
+    sortByFrequency[dict[key]] = [key];
+  }
 }
-console.log(result);
+console.log(sortByFrequency);
