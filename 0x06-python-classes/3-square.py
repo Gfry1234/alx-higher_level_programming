b8ddcd57d3027aeff58c322c4a73991cfdc6d6d0
Gfry1234 <104981860+Gfry1234@users.noreply.github.com>
2024-02-06 18:23:03 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""Defines a class Square based on private instance attribute"""
+""" creates class Square """
 
 
 class Square:
-    """Represents a new square."""
+    """ Square class"""
     def __init__(self, size=0):
-        """Initialise a new square.
-        args:
-            size(int): size of the new aquare.
-        """
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
 
     def area(self):
-        """returns the area of the square."""
-        return (self.__size * self.__size)
-
+        return self.__size * self.__size
