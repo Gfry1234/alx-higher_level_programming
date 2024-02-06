8c0ddbd836b58ca22e9e482cd5f0f5009f155553
Gfry1234 <104981860+Gfry1234@users.noreply.github.com>
2024-02-06 18:23:59 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-"""Defines a class Square based on private instance attribute"""
+""" creates class Square """
 
 
 class Square:
-    """Represents a new square."""
+    """ Square class"""
     def __init__(self, size=0):
-        """Initialise a new square.
-        args:
-            size(int): size of the new aquare.
-        """
-        self.size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     @property
     def size(self):
-        """get/set the value for the square"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def area(self):
-        """Returns the area of the square."""
-        return (self.__size * self.__size)
+        return self.__size * self.__size
